8348d2b212a283b30c83f550107c1d940fc32e4d
letoro99 <atgmatias.12@gmail.com>
2023-04-11 23:50:34 -0400

    add Elias's multiplayer code

--- scenes/game/characters/character.gd
+++ scenes/game/characters/character.gd
@@ -1,3 +1,4 @@
+class_name Character
 extends CharacterBody2D
 
 # Constants
@@ -13,6 +14,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Character Variable
 var directionMove : Vector2 
 var angleVelocity : float = 0.0 
+var initialPosition : Vector2
+
+func _ready():
+	Debug.print(name)
+	set_multiplayer_authority(name.to_int())
 
 func _handle_movement_input() -> void:
 	directionMove = Vector2.ZERO
@@ -31,37 +37,46 @@ func _handle_movement_input() -> void:
 		get_tree().root.get_node("main").add_child(element)
 
 func _handle_inputs() -> void:
-	_handle_movement_input()
+	if is_multiplayer_authority():
+		_handle_movement_input()
 	
 func transportate(in_portal: Portal, out_portal: Portal):
 	global_position = out_portal.global_position
 	if in_portal.normal_portal + out_portal.normal_portal != Vector2.ZERO:
 		var magnitude = 1.5 * velocity.length()
 		velocity = out_portal.normal_portal * magnitude
-
+		
 func _physics_process(delta):
-	_handle_inputs()
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if is_multiplayer_authority():
+		print(self.global_position)
+		_handle_inputs()
+			
+		# Add the gravity.
+		if not is_on_floor():
 
-	# Handle Jump.
-	if is_on_floor() and directionMove.y != 0:
-		velocity.y = JUMP_VELOCITY
-	
-	# Using only the horizontal velocity, sample towards the input.
-	var hvel = velocity
-	var target = directionMove * SPEED
-	var acceleration
-	hvel.y = 0
-	if directionMove.dot(hvel) > 0:
-		acceleration = ACCELERATION
-	else:
-		acceleration = DECELERATION
+			velocity.y += gravity * delta
 
-	hvel = hvel.lerp(target, acceleration * delta)
-	velocity.x = hvel.x
-	set_velocity(velocity)
+		# Handle Jump.
+		if is_on_floor() and directionMove.y != 0:
+			velocity.y = JUMP_VELOCITY
+			
+		# Using only the horizontal velocity, sample towards the input.
+		var hvel = velocity
+		var target = directionMove * SPEED
+		var acceleration
+		hvel.y = 0
+		if directionMove.dot(hvel) > 0:
+			acceleration = ACCELERATION
+		else:
+			acceleration = DECELERATION
+		hvel = hvel.lerp(target, acceleration * delta)
+		velocity.x = hvel.x
+		set_velocity(velocity)
 
-	move_and_slide()
+		move_and_slide()
+			
+		rpc("send_position",  self.global_position)
+
+@rpc("unreliable_ordered")
+func send_position(vector: Vector2) -> void:
+	global_position = vector
---
+++ scripts/Game.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var _players : Array = []
+var _thread = null
+
+signal upnp_completed(error)
+
+const PORT = 5000
+
+func _upnp_setup(server_port):
+	# UPNP queries take some time.
+	var upnp = UPNP.new()
+	var err = upnp.discover()
+
+	if err != OK:
+		push_error(str(err))
+		emit_signal("upnp_completed", err)
+		return
+
+	if upnp.get_gateway() and upnp.get_gateway().is_valid_gateway():
+		upnp.add_port_mapping(server_port, server_port, ProjectSettings.get_setting("application/config/name"), "UDP")
+		upnp.add_port_mapping(server_port, server_port, ProjectSettings.get_setting("application/config/name"), "TCP")
+		emit_signal("upnp_completed", OK)
+
+func _ready():
+	_thread = Thread.new()
+	_thread.start(_upnp_setup.bind(PORT))
+
+func _exit_tree():
+	# Wait for thread finish here to handle game exit while the thread is running.
+	_thread.wait_to_finish()
